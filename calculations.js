29ff633b020025aa566c64737bada420afde70a2
Svetlana <luchiana.sv@gmail.com>
2023-10-18 21:14:23 +0200

    added increment and decrement info - hw 2.2.4

--- calculations.js
+++ calculations.js
@@ -18,7 +18,20 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+function increaseByOne(a) {
+  a++;
+  return a;
+}
+
+function decreaseByOne(y) {
+  --y;
+  return y;
+}
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+
+console.log(increaseByOne(5));
+console.log(decreaseByOne(8));
